4c3ad720c4c45c7b78f1fde062067ee99f6a6b71
Vanessa Konynenbelt <konyn005@umn.edu>
2022-04-13 13:20:02 -0500

    add favicon

--- myapp/core/views.py
+++ myapp/core/views.py
@@ -7,6 +7,11 @@ core = Blueprint('core', __name__)
 def index():
     page = request.args.get('page', 1, type=int)
     trivias = Trivia.query.order_by(Trivia.date.desc()).paginate(page=page, per_page=5)
+    # sets=[]
+    # for trivia in trivias:
+    #     if trivia not in sets: 
+    #         sets.append(trivia)
+    # print(sets)
     return render_template('index.html', trivias=trivias)
 
 @core.route('/info')
